51dd1098b60f75940b4fbb1806afcdfc985775f4
Aishah <maishah2540@gmail.com>
2024-02-08 22:33:25 +0000

    Task 0. Done.

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,13 +1,7 @@
 /**
  * Contains a class representing a class room.
  */
-
 export default class ClassRoom {
-  /**
-   * Creates an instance of the ClassRoom classroom.
-   * @param {Number} maxStudentsSize - The maximum number of 
-   *                                    students in the class.
-   */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
